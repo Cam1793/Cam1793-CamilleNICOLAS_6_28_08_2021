074aeba7d7ef3b88f280744f0f0af7f4c1af31ad
Cam93 <camille.nicolas93@gmail.com>
2021-09-13 20:57:17 +0200

    mongodb

--- README.md
+++ README.md
@@ -2,8 +2,6 @@
 
 Ce projet a pour objectif de créer une application web de critique de sauces appelé "HOT TAKES" pour l'entreprise e-commerce Piiquante.
 
-La partie front-end de l'application est intitulé "Web-Developer-P6"
-Pour l'initialiser, vous trouverez toutes les indications nécessaire dans le README.md du dossier "Web-Developer-P6".
+La partie front-end de l'application est intitulé "Web-Developer-P6". Pour l'initialiser, vous trouverez toutes les indications nécessaire dans le README.md du dossier "Web-Developer-P6".
 
-La partie back-end de l'application est intitulé "back-end"
-Pour l'initialiser, vous trouverez toutes les indications nécessaire dans le README.md du dossier "back-end".
+La partie back-end de l'application est intitulé "back-end". Pour l'initialiser, vous trouverez toutes les indications nécessaire dans le README.md du dossier "back-end".
--- back-end/app.js
+++ back-end/app.js
@@ -16,24 +16,20 @@ const path = require('path');
 const config =  require('./config.js');
 console.log(config.DATA_BASE_NAME)
 console.log(config.DB_PASSWORD)
-console.log(config.HOST)
 
-//fonction pour lié la base de données mongoDB avec le serveur
-mongoose.connect(`mongodb+srv://${config.DB_USERNAME}:${config.DB_PASSWORD}@camille-cluster.rldpw.mongodb.net/${config.DATA_BASE_NAME}?retryWrites=true&w=majority`,
+mongoose.connect('mongodb+srv://cam1711:Mimpewdsmarkh2o@cluster0.axw5u.mongodb.net/cluster0?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
-  .catch(() => {  
-    console.log('Connexion à MongoDB échouée !')}
-    );
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-  //CORS
-  app.use((req, res, next) => {
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
+
 
 app.use(bodyParser.json());
 
