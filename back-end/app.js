a12963a1eebc267e58b7c51555545a2ce312e53c
Cam93 <camille.nicolas93@gmail.com>
2021-09-14 18:41:55 +0200

    modif app and like

--- back-end/app.js
+++ back-end/app.js
@@ -26,7 +26,7 @@ mongoose.connect(`mongodb+srv://${config.DB_USERNAME}:${config.DB_PASSWORD}@cami
     console.log('Connexion à MongoDB échouée !')}
     );
 
-  //CORS Cross-Origin Resource Sharing
+  //CORS (Cross-Origin Resource Sharing)
   app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
